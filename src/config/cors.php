b5e1cd462570477a64ad4d88e66c8090c9c708c8
Abdallah Al Sawaqi <buzz644@yahoo.com>
2025-10-13 18:37:17 +0400

    updated the cors

--- src/config/cors.php
+++ src/config/cors.php
@@ -19,7 +19,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
                           'http://localhost:84',
-                          'https://abdallahweb.com'
+                          'https://avinaq.com'
                          ], // Frontend URL
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
